935a745cf22bfd212522a821cbbe37b099b0c6d7
tnt108 <tnt108@gmail.com>
2018-04-30 23:36:44 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
-ActionController::Routing::Routes.draw do |map|
-  map.connect '/importer/index', :controller => 'importer', :action => 'index'
-  map.connect '/importer/match', :controller => 'importer', :action => 'match'
+if Rails::VERSION::MAJOR >= 3
+  RedmineApp::Application.routes.draw do
+    match '/importer/index', :to => 'importer#index', :via => [:get, :post]
+    match '/importer/index', :to => 'importer#match', :via => [:get, :post]
+    match '/importer/index', :to => 'importer#result', :via => [:get, :post]
+  end
+else
+  ActionController::Routing::Routes.draw do |map|
+    map.connect '/importer/index', :controller => 'importer', :action => 'index'
+    map.connect '/importer/match', :controller => 'importer', :action => 'match'
+    map.connect '/importer/result', :controller => 'importer', :action => 'result'
+  end
 end
